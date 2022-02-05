2b50f45bb58c9f730eae409d8e9f372c4108d1ca
andriusmw <andriusmw@yahoo.es>
2022-02-05 11:53:23 +0100

    añadido hero.ts

--- portfolioangular1/src/app/heroes/heroes.component.ts
+++ portfolioangular1/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  hero = 'Windstorm';
+
 
 }
