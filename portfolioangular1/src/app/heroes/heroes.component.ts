772086af2edd5262cb2e0f3a4159934896e78896
andriusmw <andriusmw@yahoo.es>
2022-02-07 09:04:02 +0100

    añadida función onSelect

--- portfolioangular1/src/app/heroes/heroes.component.ts
+++ portfolioangular1/src/app/heroes/heroes.component.ts
@@ -17,7 +17,16 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;  //metemos la propiedad heroes en el export para mostrar el contenido del array HEROES
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+         }
 
+         //De alguna manera esto significa que cuando se active la función onSelect, la variable
+       //  selectedHero tendrá el valor del let hero en el que se encuentre. Pero que empieza con valor
+       // void, vacío, dentro de onSelect() del paréntesisn se ponen las variables a usar. y arriba
+       // del todo es lo mismo, dice que la variable selectedHero tendrá el valor de Hero, pero igualmente
+       //ese no tiene valor hasta que no se lo pase la funcion onSelect.
 
   constructor() { }
 
