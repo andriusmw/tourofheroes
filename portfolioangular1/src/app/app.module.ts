79dfd55dc3d2c2922334f6deeb1d1fa0b679fb9b
andriusmw <andriusmw@yahoo.es>
2022-02-08 14:51:44 +0100

    añadido hero-detail

--- portfolioangular1/src/app/app.module.ts
+++ portfolioangular1/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 //añade las librerias de formularios de angular
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
