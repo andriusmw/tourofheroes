66eadf2a02f978e60763829b8a43e2a2b12c723e
andriusmw <andriusmw@yahoo.es>
2022-02-08 15:41:07 +0100

    creando servicio hero 2 anotaciones

--- portfolioangular1/src/app/hero.service.ts
+++ portfolioangular1/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from './hero'; //Importa la clase Hero de hero.ts (el id y el nombre)
+import { HEROES } from './mock-heroes'; //Importa HEROES del mock-heroes.ts (el listado de héroes)
 
 @Injectable({
   providedIn: 'root'
