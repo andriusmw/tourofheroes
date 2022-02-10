fe882549bb40d4ed45e07953551da96efc7f7b99
andriusmw <andriusmw@yahoo.es>
2022-02-10 17:02:09 +0100

    añadida ruta al routing module

--- portfolioangular1/src/app/app-routing.module.ts
+++ portfolioangular1/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component'; //Añado el import
+import { DashboardComponent } from './dashboard/dashboard.component'; //añadido el dashbord
 
 const routes: Routes = [ //Routes le dice al Router a donde ir cuando se clickea.
-  { path: 'heroes', component: HeroesComponent } //Path indica lo que se añade a la URL
-];                                               // Component: indica el componente a cargar cuando
+  { path: 'heroes', component: HeroesComponent }, //Path indica lo que se añade a la URL
+                                               // Component: indica el componente a cargar cuando
                                                 // se añade la ruta, el path.
 
+
+  { path: 'dashboard', component: DashboardComponent } //Añadida ruta del dashboard
+
+ ];
  /*
  This tells the router to match that URL to path: 'heroes' and display
  the HeroesComponent when the URL is something like localhost:4200/heroes.
