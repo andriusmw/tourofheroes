726d58d88666cf255efeef713b87611466ef9905
andriusmw <andriusmw@yahoo.es>
2022-02-10 16:30:54 +0100

    modificando app routing.ts

--- portfolioangular1/src/app/app-routing.module.ts
+++ portfolioangular1/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component'; //Añado el import
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent } //Añado las rutas ??
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
