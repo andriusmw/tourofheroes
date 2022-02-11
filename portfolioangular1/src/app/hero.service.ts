dc087c6d2042be9e18c7c430e82af26a2ef9e24a
andriusmw <andriusmw@yahoo.es>
2022-02-11 15:55:19 +0100

    modificado hero.service para obtener el id del mediante routing

--- portfolioangular1/src/app/hero.service.ts
+++ portfolioangular1/src/app/hero.service.ts
@@ -17,6 +17,16 @@ export class HeroService { //Esporta el servicio
     return heroes;
   }
 
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }  //Añadido para mostrar el detalle mediante routing
+
+
+
   constructor(private messageService: MessageService) { } //Se declara el servicio en el constructor
 }
 
