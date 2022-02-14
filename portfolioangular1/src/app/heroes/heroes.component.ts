b453e6994a97510a402f5c28008aa13e294cb33b
andriusmw <andriusmw@yahoo.es>
2022-02-14 14:29:15 +0100

    añadidos comentarios add()

--- portfolioangular1/src/app/heroes/heroes.component.ts
+++ portfolioangular1/src/app/heroes/heroes.component.ts
@@ -41,5 +41,24 @@ export class HeroesComponent implements OnInit {
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  /*-
+  Declaramos el método add(). que se le llama desde el input de add hero.
+
+  When the given name is non-blank, the handler creates a Hero-like object from
+  the name (it's only missing the id) and passes it to the services addHero() method.
+
+When addHero() saves successfully, the subscribe() callback receives the new hero and
+pushes it into to the heroes list for display.
+
+  -*/
+
 
 }
